4ed9513b3c7b90bde76b000b2cf4f1d279eed627
DUDIGAM VENKATA SAI AKASH <sai.sai72077@gmail.com>
2026-08-13 22:51:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (11.15%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,22 +1,11 @@
 class Solution {
     public int pivotInteger(int n) {
-        int prefixSum=1;
-        int suffixSum=n;
-        int i=1, j=n;
-        while(i<j){
-            if(prefixSum<suffixSum){
-                i++;
-                prefixSum+=i;
-            }else{
-                j--;
-                suffixSum+=j;
-            }
-        }
+        int sum = (n*(n+1))/2;
+        int x=(int)Math.sqrt(sum);
 
-        if(suffixSum==prefixSum){
-            return i;
+        if(x*x==sum){
+            return x;
         }
-
         return -1;
     }
 }
