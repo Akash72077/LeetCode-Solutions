65457e0d18f772e4b1485484280fd0ba39bc7e85
DUDIGAM VENKATA SAI AKASH <sai.sai72077@gmail.com>
2026-08-13 15:54:32 +0530

    Time: 75 ms (36.51%) | Memory: 111.2 MB (57.59%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -4,7 +4,7 @@ public:
         unordered_set<int> s;
         int n=nums.size();
         for(int i=0; i<n; i++){
-            if(s.count(nums[i])){
+            if(s.find(nums[i])!=s.end()){
                 return true;
             }
             s.insert(nums[i]);
