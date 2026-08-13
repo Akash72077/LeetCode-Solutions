07859b5a852619328b18075413ecf797c28a5f8a
DUDIGAM VENKATA SAI AKASH <sai.sai72077@gmail.com>
2026-08-13 15:45:28 +0530

    Time: 17 ms (77.86%) | Memory: 107.8 MB (54.50%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,9 +3,10 @@ class Solution {
         HashSet<Integer> hs= new HashSet<>();
         int n=nums.length;
         for(int i=0; i<n; i++){
-            if(!hs.add(nums[i])){
+            if(hs.contains(nums[i])){
                 return true;
             }
+            hs.add(nums[i]);
         }
         return false;
     }
