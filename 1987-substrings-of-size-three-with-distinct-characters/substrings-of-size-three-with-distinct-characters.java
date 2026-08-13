093cd359f6df685e646908bb74baa3566c2bc249
DUDIGAM VENKATA SAI AKASH <sai.sai72077@gmail.com>
2026-08-13 10:55:48 +0530

    Time: 1 ms (97.32%) | Memory: 42.8 MB (81.77%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -1,24 +1,16 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        int n=s.length();
-        if(n<3){
-            return 0;
-        }
-    int count =0;
-    int left =0;
-    int right=2;
-       
-        while(right<n){
-            char a = s.charAt(left);
-            char b = s.charAt(left+1);
-            char c = s.charAt(right);
-            if(a!=b && b!=c && c!=a){
-                count++;
-            }
+    int n=s.length();
+    int count=0;
+    for(int left =0; left<n-2; left++){
+        char a= s.charAt(left);
 
-            left++;
-            right++;
+        char b= s.charAt(left+1);
+        char c= s.charAt(left+2);
+        if(a!=b && b!=c && c!=a){
+            count ++;
         }
-        return count;
+    }
+    return count;
     }
 }
