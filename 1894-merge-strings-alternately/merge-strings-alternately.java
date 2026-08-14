007aaa1c3213e8de505f467183413ab95bec93f9
DUDIGAM VENKATA SAI AKASH <sai.sai72077@gmail.com>
2026-08-14 22:21:05 +0530

    Time: 4 ms (19.31%) | Memory: 44 MB (10.67%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,30 +1,23 @@
 class Solution {
     public String mergeAlternately(String s, String t) {
-        int i=s.length()-1;
-        int j=t.length()-1;
-        String ans="";
-        int k=0;
-        int l=0;
-        while(i>=0 && j>=0){
-            ans+=s.charAt(k++);
-            ans+=t.charAt(l++);
-            i--;
-            j--;
-        }
-        if(i>=0){
-             while(i>=0){
-            ans+=s.charAt(k++);
-            i--;
-        }
-        }else{
 
-            while(j>=0){
-            
-            ans+=t.charAt(l++);
-            j--;
+        int i = 0;
+        int j = 0;
+        String ans = "";
+
+        while (i < s.length() && j < t.length()) {
+            ans += s.charAt(i++);
+            ans += t.charAt(j++);
         }
 
+        while (i < s.length()) {
+            ans += s.charAt(i++);
         }
+
+        while (j < t.length()) {
+            ans += t.charAt(j++);
+        }
+
         return ans;
     }
 }
