04155a3c160dbbfd87c5929337eb715f86b702cf
DUDIGAM VENKATA SAI AKASH <sai.sai72077@gmail.com>
2026-08-14 13:24:01 +0530

    Time: 1 ms (99.06%) | Memory: 42.7 MB (15.27%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -1,16 +1,5 @@
 class Solution {
     public boolean isUgly(int n) {
-        // ArrayList<Integer> ans= new ArrayList<>();
-        // if(n==1 || n==2|| n==3|| n==5){
-        //     return true;
-        // }
-        // for(int i=4; i<=n/2; i++){
-        //         if(n%i==0){
-        //             return false;
-        //         }
-        // }
-       
-        // return true;
      while(n>1){
             if(n%2==0){
                 n/=2;
