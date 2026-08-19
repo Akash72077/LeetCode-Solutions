693280ff653cc5b5329bd1751f96a93001377bd2
DUDIGAM VENKATA SAI AKASH <sai.sai72077@gmail.com>
2026-08-20 00:10:36 +0530

    Time: 1 ms (77.42%) | Memory: 46.7 MB (64.04%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,13 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-        
-        int k = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[k - 1]) {
-                nums[k++] = nums[i];
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
             }
-        }
-        return k;
+        }return i+1;
     }
 }
