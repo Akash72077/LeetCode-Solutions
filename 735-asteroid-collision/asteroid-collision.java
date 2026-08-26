6f1d9e0d25cd5e3d679f5e49468d67ad245612d9
DUDIGAM VENKATA SAI AKASH <sai.sai72077@gmail.com>
2026-08-26 23:40:29 +0530

    Time: 5 ms (67.28%) | Memory: 47.2 MB (29.74%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,32 +1,28 @@
 class Solution {
     public int[] asteroidCollision(int[] nums) {
-        int n= nums.length;
+        int n = nums.length;
         Stack<Integer> st = new Stack<>();
         for(int i=0; i<n; i++){
             int stone = nums[i];
-           if(st.isEmpty()|| stone>0){
-             st.push(stone);
-             }
-             else{
-               
-                    while(!st.isEmpty() && Math.abs(stone)>st.peek() && st.peek() >0){
-                         st.pop();
-                      
-                    }
-                   
-                if(st.isEmpty()  || st.peek()<0){ 
+            if(st.isEmpty() ||  stone>0){
+                st.push(stone);
+            }else{
+                while(!st.isEmpty() && st.peek()<Math.abs(stone)&& st.peek()>0 ){
+                    st.pop();
+                }
+                if(st.isEmpty() || st.peek()<0){
                     st.push(stone);
-                }else if(st.peek()== Math.abs(stone)){
+                }else if(st.peek() == Math.abs(stone)){
                     st.pop();
                 }
             }
+           
         }
-        int arr[]= new int[st.size()];
-        int i=0;
-        for(int num: st){
-            arr[i]=num;
-            i++;
-        }
-        return arr;
+         int []res= new int[st.size()];
+            int i=0;
+            for(int num : st){
+                res[i++]=num;
+            }
+        return res;
     }
 }
