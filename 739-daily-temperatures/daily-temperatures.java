359eb70f93e1c968c1111211b51cfa14a19f6ff4
DUDIGAM VENKATA SAI AKASH <sai.sai72077@gmail.com>
2026-08-27 14:37:39 +0530

    Time: 80 ms (9.28%) | Memory: 106.9 MB (55.69%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -4,20 +4,14 @@ class Solution {
         int n= temperatures.length;
         int []ans = new int[n];
         for(int i=n-1; i>=0; i--){  
-
             while(!st.isEmpty() && temperatures[st.peek()]<= temperatures[i]){
                 st.pop();
-            }
-            
+            }            
             if(!st.isEmpty()){
                 ans[i]=st.peek()-i;
             }
             st.push(i);
-
         }
-
-
-
         return ans;
     }
 }
