9c871701ee487d0fca716a268bf1f285842e1b31
DUDIGAM VENKATA SAI AKASH <sai.sai72077@gmail.com>
2026-08-27 17:57:58 +0530

    Time: 1 ms (96.90%) | Memory: 47.2 MB (60.04%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.java
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -1,18 +1,17 @@
 
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-
         ListNode dummy = new ListNode(-1);
-        dummy.next=head;
+        dummy.next = head;
         ListNode temp = dummy;
+
         while(temp!=null && temp.next!=null){
             if(temp.next.val==val){
-                temp.next=temp.next.next;
+                temp.next = temp.next.next;
             }else{
-                temp=temp.next;
+                temp = temp.next;
             }
         }
         return dummy.next;
-        
     }
 }
